9c3f54e143beb7689d7670e9ffea52f05b7e0206
Anonshack <anonshack48@gmail.com>
2025-10-27 11:05:46 +0500

    update

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-# import socket
-#
-# s=socket.socket()
-# s.connect(("192.168.1.1", 22))
-#
-# answer=s.recv(1024)
-# print(answer)
-#
-# s.close
+import requests
+from json import dump
+from threading import Thread
+
+urls = ["https://dummyjson.com/posts", 'https://dummyjson.com/posts', "https://dummyjson.com/posts"]
+
+
+def download_and_save(url, name):
+    response = requests.get(url)
+    data = response.json()
+    with open(name, "w", encoding="utf-8") as f:
+        dump(data, f, indent=4, ensure_ascii=False)
+
+
+threads = []
+num = 0
+
+for url in urls:
+    num += 1
+    proccess = Thread(target=download_and_save, args=(url, f'file{num}.json'))
+    proccess.start()
+    threads.append(proccess)
+for el in threads:
+    el.join()
 
 
