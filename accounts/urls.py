b7e212db5e1c2ae0f3ac84a60d94d29170b31cd8
Anonshack <anonshack48@gmail.com>
2025-11-01 21:14:01 +0500

    update

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,6 +9,7 @@ from accounts.auth.user_profile import (
 urlpatterns = [
     path('all-users/', GetAllUsers.as_view(), name='get_all_users'),
     path('profile/', ProfileView.as_view(), name='profile'),
+
     # profile dan turib passwordni change qilish
     path('change-password/', ChangePasswordView.as_view(), name='change-password'),
     path('countries/', CountryListView.as_view(), name='countries'),
--- geo/views.py
+++ geo/views.py
@@ -1,4 +1,3 @@
-from drf_spectacular.utils import extend_schema
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.views import APIView
 from rest_framework.response import Response
