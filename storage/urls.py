d31c3be6e4201f62229eb4755757cd7bd60bb7d2
Anonshack <anonshack48@gmail.com>
2025-11-03 11:41:57 +0500

    update

--- storage/urls.py
+++ storage/urls.py
@@ -1,3 +1,8 @@
 from django.urls import path
+from .views import UploadFileView, UserFilesListView, PublicFileRetrieveView
 
-urlpatterns = []
+urlpatterns = [
+    path('upload/', UploadFileView.as_view(), name='storage-upload'),
+    path('me/', UserFilesListView.as_view(), name='storage-my-files'),
+    path('<int:pk>/', PublicFileRetrieveView.as_view(), name='storage-file-detail'),
+]
