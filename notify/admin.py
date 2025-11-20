7a4ecd0d6cf436a4e94fe6f7187899479684600a
Anonshack <anonshack48@gmail.com>
2025-11-20 20:33:10 +0500

    update

--- notify/admin.py
+++ notify/admin.py
@@ -5,9 +5,7 @@ from django.utils.html import format_html
 from .models import Notification
 from accounts.models import User
 
-# ----------------------------
-# Resource for import-export
-# ----------------------------
+
 class NotificationResource(resources.ModelResource):
     class Meta:
         model = Notification
@@ -20,21 +18,16 @@ class NotificationResource(resources.ModelResource):
             "created_at",
         )
 
-# ----------------------------
-# Notification admin with import-export and statistics
-# ----------------------------
+
 @admin.register(Notification)
 class NotificationAdmin(ImportExportModelAdmin):
     resource_class = NotificationResource
 
-    # Display columns in admin
     list_display = ("get_user_email", "title", "is_read", "created_at", "notification_stats")
     search_fields = ("user__email", "title", "message")
     list_filter = ("is_read", "created_at")
 
-    # ----------------------------
-    # Show general statistics above changelist
-    # ----------------------------
+
     def changelist_view(self, request, extra_context=None):
         extra_context = extra_context or {}
 
@@ -52,17 +45,13 @@ class NotificationAdmin(ImportExportModelAdmin):
 
         return super().changelist_view(request, extra_context=extra_context)
 
-    # ----------------------------
-    # Custom display for user email
-    # ----------------------------
+
     def get_user_email(self, obj):
         return obj.user.email if obj.user else "-"
     get_user_email.short_description = "User Email"
     get_user_email.admin_order_field = "user"
 
-    # ----------------------------
-    # Custom method to show statistics in list_display
-    # ----------------------------
+
     def notification_stats(self, obj):
         stats = {
             "total_notifications": Notification.objects.count(),
