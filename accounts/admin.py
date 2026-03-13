21d2577e52f3c1abac8f7ecfd041d29d8fdac83f
Anonshack <anonshack48@gmail.com>
2026-03-13 19:58:32 +0500

    update

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,7 +2,7 @@
 # from import_export import resources
 # from import_export.admin import ImportExportModelAdmin
 # from .models import User
-#
+
 # class UserResource(resources.ModelResource):
 #     class Meta:
 #         model = User
--- ai_tools/views.py
+++ ai_tools/views.py
@@ -9,6 +9,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 import openai
 
+# with ai
 openai.api_key = "YOUR_OPENAI_API_KEY"
 
 dynamic_apis = {}
