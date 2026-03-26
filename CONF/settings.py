c6aed3d8aee8acb15cf62c5eae8f12e1e0e00bf8
Anonshack <anonshack48@gmail.com>
2026-03-26 16:10:02 +0500

    update

--- CONF/settings.py
+++ CONF/settings.py
@@ -86,8 +86,11 @@ DATABASES = {
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST', default='127.0.0.1'),
-        'PORT': config('DB_PORT', default='5432'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', cast=int),
+        'OPTIONS': {
+            'sslmode': config('DB_SSLMODE', default='require')
+        },
     }
 }
 print("DB:", config("DB_NAME"))
