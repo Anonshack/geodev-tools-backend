e4d339eab8b62f6e46bc485af413efc2806b8f81
Anonshack <anonshack48@gmail.com>
2026-03-26 15:56:53 +0500

    update

--- CONF/settings.py
+++ CONF/settings.py
@@ -12,7 +12,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = config('DEBUG', default=True, cast=bool)
 # ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
 ALLOWED_HOSTS = [
-                 "django-api-q4xhp.ondigitalocean.app",
+"django-api-q4xhp.ondigitalocean.app",
+"127.0.0.1",
                  ]
 
 SECRET_KEY = config('SECRET_KEY')
